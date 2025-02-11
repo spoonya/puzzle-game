611b8686f98c9b836d9945220b5bcb02b8a6ebc3
spoonya <nomad8t@gmail.com>
2025-02-11 13:28:34 +0300

    feat: add img preview

--- src/ts/modal.ts
+++ src/ts/modal.ts
@@ -1,27 +1,27 @@
-import { header, puzzleDOM } from './index';
+import { header, puzzleDOM } from "./index";
 
 export default class Modal {
-  private puzzleWrapper = document.querySelector('.puzzle-wrapper');
-  private _modalBg: HTMLElement = document.createElement('div');
-  private _modal: HTMLElement = document.createElement('div');
-  private _modalInner: HTMLElement = document.createElement('div');
-  private _modalTitle: HTMLElement = document.createElement('h3');
-  private _modalMsg: HTMLElement = document.createElement('p');
+  private puzzleWrapper = document.querySelector(".puzzle-wrapper");
+  private _modalBg: HTMLElement = document.createElement("div");
+  private _modal: HTMLElement = document.createElement("div");
+  private _modalInner: HTMLElement = document.createElement("div");
+  private _modalTitle: HTMLElement = document.createElement("h3");
+  private _modalMsg: HTMLElement = document.createElement("p");
 
   public static alertType = {
     onGameEnd: {
-      title: 'Grats!',
+      title: "Grats!",
       getStats(): string {
-        return `You won the game in ${header.movesCount} moves! You've spent ${header.time} and you solved ${puzzleDOM.boardSize}x${puzzleDOM.boardSize} puzzle!`
+        return `You won the game in ${header.movesCount} moves! You've spent ${header.time} and you solved ${puzzleDOM.boardSize}x${puzzleDOM.boardSize} puzzle!`;
       },
     },
     onGameSave: {
-      title: 'Game successfully saved!'
+      title: "Game successfully saved!",
     },
     onSaveDelete: {
-      title: 'Save successfully deleted!'
-    }
-  }
+      title: "Save successfully deleted!",
+    },
+  };
 
   constructor(type: object) {
     switch (type) {
@@ -40,18 +40,18 @@ export default class Modal {
         break;
 
       default:
-        new Error('Wrong alert type');
+        new Error("Wrong alert type");
         break;
     }
   }
 
-  public showAlert(delay: number = 0): Modal {
+  public showAlert(delay = 0): Modal {
     setTimeout(() => {
-      this._modalBg.classList.add('modal-bg');
-      this._modal.classList.add('modal');
-      this._modalInner.classList.add('modal__inner');
-      this._modalTitle.classList.add('modal__title');
-      this._modalMsg.classList.add('modal__msg');
+      this._modalBg.classList.add("modal-bg");
+      this._modal.classList.add("modal");
+      this._modalInner.classList.add("modal__inner");
+      this._modalTitle.classList.add("modal__title");
+      this._modalMsg.classList.add("modal__msg");
 
       this.puzzleWrapper?.append(this._modalBg);
       this._modalBg.append(this._modal);
--- src/ts/puzzle-dom.ts
+++ src/ts/puzzle-dom.ts
@@ -1,12 +1,13 @@
-import {
-  images
-} from './images';
+import { images } from "./images";
 
 export default class PuzzleDOM {
-  private _boardSize: number = 0;
+  private _boardSize = 0;
   private _puzzleItem: HTMLElement | null = null;
-  public puzzle: HTMLElement = document.createElement('div');
-  private _puzzleWrapper: HTMLElement | null = document.querySelector('.puzzle-wrapper');
+  private _puzzlePreview: HTMLElement | null =
+    document.querySelector(".puzzle-preview");
+  public puzzle: HTMLElement = document.createElement("div");
+  private _puzzleWrapper: HTMLElement | null =
+    document.querySelector(".puzzle-wrapper");
   public numArr: number[] = [];
   public solvedNumArr: number[] = [];
   public cellsArr: HTMLElement[] = [];
@@ -20,7 +21,7 @@ export default class PuzzleDOM {
     if (val >= 2 && val <= 8) {
       this._boardSize = Math.round(val);
     } else {
-      throw new Error('Invalid board size');
+      throw new Error("Invalid board size");
     }
   }
 
@@ -31,17 +32,20 @@ export default class PuzzleDOM {
 
     return {
       col: val % totalCols === 0 ? totalCols : val % totalCols,
-      row: Math.ceil(val / totalCols)
-    }
+      row: Math.ceil(val / totalCols),
+    };
   }
 
   private _chooseRandImg(min: number, max: number): string {
     const randNum = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    return images[randNum.toString() + '.jpg'];
+    return images[randNum.toString() + ".jpg"];
   }
 
-  private _createRandArray(arrLength: number, isForInitScreen = false): number[] {
+  private _createRandArray(
+    arrLength: number,
+    isForInitScreen = false
+  ): number[] {
     const arr: number[] = [];
     arrLength = Math.pow(arrLength, 2);
 
@@ -64,8 +68,8 @@ export default class PuzzleDOM {
 
   private _isSovlable(): boolean {
     const puzzleLength: number = this.numArr.length - 1;
-    let sum: number = 0;
-    let zeroIdx: number = 0;
+    let sum = 0;
+    let zeroIdx = 0;
 
     for (let i = 0; i < puzzleLength; i++) {
       if (this.numArr[i] !== 0) {
@@ -79,7 +83,7 @@ export default class PuzzleDOM {
       }
     }
 
-    this.boardSize % 2 === 0 ? sum += this.calcCoords(zeroIdx).row : false;
+    this.boardSize % 2 === 0 ? (sum += this.calcCoords(zeroIdx).row) : false;
     return sum % 2 === 0;
   }
 
@@ -89,18 +93,35 @@ export default class PuzzleDOM {
     }
   }
 
-  public createPuzzle(puzzleStyle: boolean, objForLoad ? : any): void {
+  private _createPreview(): void {
+    if (!this._puzzlePreview || !this.img) return;
+
+    this._puzzlePreview.style.background = `url('${this.img}')`;
+    this._puzzlePreview.style.backgroundSize = "cover";
+    this._puzzlePreview.style.width = "150px";
+    this._puzzlePreview.style.height = "150px";
+    this._puzzlePreview.style.border = "2px solid #333";
+    this._puzzlePreview.style.borderRadius = "5px";
+    this._puzzlePreview.style.margin = "10px auto";
+  }
+
+  public createPuzzle(puzzleStyle: boolean, objForLoad?: any): void {
     this._cleanBoard();
+
+    if (this._puzzlePreview) {
+      this._puzzlePreview.style.display = puzzleStyle ? "block" : "none";
+    }
 
     this.img = null;
     this.cellsArr = [];
 
-    this.puzzle.classList.add('puzzle');
+    this.puzzle.classList.add("puzzle");
 
     this._puzzleWrapper?.append(this.puzzle);
 
     if (!objForLoad && puzzleStyle === true) {
       this.img = this._chooseRandImg(1, 150);
+      this._createPreview();
     }
 
     if (objForLoad) {
@@ -124,25 +145,26 @@ export default class PuzzleDOM {
     const arrLength = this.numArr.length;
 
     for (let i = 0; i < arrLength; i++) {
-      this._puzzleItem = document.createElement('div');
+      this._puzzleItem = document.createElement("div");
 
       if (this.numArr[i] === 0) {
-        this._puzzleItem.classList.add('puzzle__empty');
+        this._puzzleItem.classList.add("puzzle__empty");
       } else {
         if (puzzleStyle) {
-          this._puzzleItem.style.color = 'transparent';
-          this._puzzleItem.style.textShadow = 'none';
+          this._puzzleItem.style.color = "transparent";
+          this._puzzleItem.style.textShadow = "none";
           this._puzzleItem.style.background = `url('${this.img}')`;
           this._puzzleItem.style.backgroundSize = `${100 * this.boardSize}%`;
-          this._puzzleItem.style.backgroundPosition =
-            `${ (this.calcCoords(this.numArr[i]).col - 1) * imgPart }%
-             ${ (this.calcCoords(this.numArr[i]).row - 1) * imgPart }%`;
+          this._puzzleItem.style.backgroundPosition = `${
+            (this.calcCoords(this.numArr[i]).col - 1) * imgPart
+          }%
+             ${(this.calcCoords(this.numArr[i]).row - 1) * imgPart}%`;
         }
-        this._puzzleItem.classList.add('puzzle__item');
-        this._puzzleItem.innerText = (this.numArr[i]).toString();
+        this._puzzleItem.classList.add("puzzle__item");
+        this._puzzleItem.innerText = this.numArr[i].toString();
       }
 
-      this._puzzleItem.setAttribute('id', this.numArr[i].toString());
+      this._puzzleItem.setAttribute("id", this.numArr[i].toString());
 
       this.puzzle.append(this._puzzleItem);
 
@@ -155,20 +177,20 @@ export default class PuzzleDOM {
 
     this._createRandArray(4, true);
 
-    this.puzzle.style.gridTemplateColumns = `repeat(4, 1fr)`;
+    this.puzzle.style.gridTemplateColumns = "repeat(4, 1fr)";
 
-    this.puzzle.classList.add('puzzle');
+    this.puzzle.classList.add("puzzle");
 
     this._puzzleWrapper?.append(this.puzzle);
 
     for (let i = 0; i < this.solvedNumArr.length; i++) {
-      this._puzzleItem = document.createElement('div');
+      this._puzzleItem = document.createElement("div");
 
       if (this.solvedNumArr[i] === 0) {
-        this._puzzleItem.classList.add('puzzle__empty');
+        this._puzzleItem.classList.add("puzzle__empty");
       } else {
-        this._puzzleItem.classList.add('puzzle__item');
-        this._puzzleItem.innerText = (this.solvedNumArr[i]).toString();
+        this._puzzleItem.classList.add("puzzle__item");
+        this._puzzleItem.innerText = this.solvedNumArr[i].toString();
       }
 
       this.puzzle.append(this._puzzleItem);
